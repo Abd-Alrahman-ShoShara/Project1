875f8a8e1ffb32ce0912e951168b624fc17d3c05
Abdsi8a <abdsii8a@gmail.com>
2024-05-24 19:20:49 +0300

    d

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -61,6 +61,7 @@ class TicketController extends Controller
         foreach ($alreadyTickets as $alreadyTicket) {
             $numOfFlights+=1;
         }
+        
 
         return response()->json([
             'message' => 'There are already tickets',

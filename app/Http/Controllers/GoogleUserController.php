433611696d7fd9ba7ac134603de2cf78989a070d
Abdsi8a <abdsii8a@gmail.com>
2024-06-29 11:03:22 +0300

    t

--- app/Http/Controllers/GoogleUserController.php
+++ app/Http/Controllers/GoogleUserController.php
@@ -5,37 +5,46 @@ namespace App\Http\Controllers;
 use App\Models\GoogleUser;
 use App\Models\User;
 use Illuminate\Http\Request;
-use PhpParser\Node\Expr\FuncCall;
 
 class GoogleUserController extends Controller
 {
-    public function googleRegister(Request $request){
-        $atter = $request->validate([
-            'name'=> 'required|max:255',
-            'email'=> 'required|email|unique:google_users,email',
-            'google_id'=>'required|unique:google_users',
-        ]);
-        
-        $user=User::create([
-            'name'=>$request->name,
-            'type'=>'google',          
+    public function googleRegister(Request $request)
+{
+    $attr = $request->validate([
+        'name' => 'required|max:255',
+        'email' => 'required|email',
+        'google_id' => 'required',
+    ]);
+
+    $googleUser = GoogleUser::where('email', $attr['email'])->first();
+
+    if ($googleUser) {
+        $token = $googleUser->user->createToken('auth_token')->accessToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'user' => $googleUser->load('user'),
+            'token' => $token,
+        ], 200);
+    } else {
+        $user = User::create([
+            'name' => $attr['name'],
+            'type' => 'google',
         ]);
 
-        $googleUser=GoogleUser::create([
-            'user_id'=>$user->id,
-            'email'=>$request->email,
-            'google_id'=>$request->google_id,
+        $googleUser = GoogleUser::create([
+            'user_id' => $user->id,
+            'email' => $attr['email'],
+            'google_id' => $attr['google_id'],
         ]);
-        if(!$googleUser){
-            return response()->json([
-                'message'=>'The provided credentials are incorrect'
-            ],404);
-        }
-        $token=$user->createToken('auth_token')->accessToken;
 
-        return response([
-            'message'=>'register successfully',
-            'token'=>$token
-        ],200);
+        $token = $user->createToken('auth_token')->accessToken;
+
+        return response()->json([
+            'message' => 'Registration successful',
+            'user' => $googleUser->load('user'),
+            'token' => $token,
+        ], 200);
     }
+}
 }

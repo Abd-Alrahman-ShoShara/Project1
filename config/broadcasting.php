000170dd0af589a6d50e01c73e4ace02dba8913d
Abdsi8a <abdsii8a@gmail.com>
2024-08-14 12:04:57 +0300

    d

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -31,19 +31,21 @@ return [
     'connections' => [
 
 'pusher' => [
-    'driver' => 'pusher',
-    'key' => env('PUSHER_APP_KEY'),
-    'secret' => env('PUSHER_APP_SECRET'),
-    'app_id' => env('PUSHER_APP_ID'),
-    'options' => [
-        'host' => env('PUSHER_HOST', 'api-mt1.pusher.com'), // Use default if not set in environment
-        'port' => env('PUSHER_PORT', 443),
-        'scheme' => env('PUSHER_SCHEME', 'https'),
-        'encrypted' => true,
-        'useTLS' => true, // Set to true for HTTPS
-        'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
-    ],
-],
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
+                'port' => env('PUSHER_PORT', 443),
+                'scheme' => env('PUSHER_SCHEME', 'https'),
+                'encrypted' => true,
+                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+            ],
+            'client_options' => [
+                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+            ],
+        ],
         'ably' => [
             'driver' => 'ably',
             'key' => env('ABLY_KEY'),
